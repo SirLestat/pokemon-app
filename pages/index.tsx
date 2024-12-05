f9e2da3aa9175bd0a0c266c9b849cceb699900a3
Edgar Alejandro Santiago Perez <alejandrosantiago.cfeteit@gmail.com>
2024-12-04 20:03:02 -0600

    Primer commit 🎉

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@nextui-org/react";
+import { NextPage } from "next";
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Button color="secondary" variant="shadow">
+        Click Me
+      </Button>
+    </>
+  );
+};
+
+export default HomePage;
